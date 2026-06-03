04901b8851abf95d9fc2f91a2814a008499a4003
Mrunalighadge <mrunalighadge1631@gmail.com>
2026-06-04 00:06:39 +0530

    Removed broken Grafana embeds from not page

--- frontend/src/pages/NotificationMonitoring.js
+++ frontend/src/pages/NotificationMonitoring.js
@@ -247,9 +247,7 @@ function NotificationMonitoring() {
 
         </div>
 
-        {/* NOTIFICATION CHARTS */}
-
-<div className="grid grid-cols-1 lg:grid-cols-2 gap-8 mb-10">
+        
 
   {/* NOTIFICATION DASHBOARD */}
 
